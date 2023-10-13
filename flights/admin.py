8c2d41fd02d405b763938da41b13bd71fa243ab0
nikhil123 <its.nikhil2808@gmail.com>
2023-10-14 00:41:58 +0530

    first commit

---
+++ flights/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Flight,Airport
+
+# admin.site.Register(Airport)
+# admin.site.Register(Flight)
---
+++ flights/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Airport(models.Model):
+    code=models.CharField(max_length=3)
+    city=models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+
+class Flight(models.Model):
+    origin=models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="departures")
+    destination=models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="arrivals")
+    # duration=models.IntegerField()
+
+    def __str__(self):
+        return  f"Flight fromm {self.origin} to {self.destination}"
+
+
+
+# drf __str__ reprsents a string representation of a 
+#Foreing key -one to many, referenec to another table
